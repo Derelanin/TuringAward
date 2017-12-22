b276adac3651a2e759cd4a5f9021e41dde474b16
Derelanin <solo.akicare@gmail.com>
2017-12-22 18:42:33 +0300

    Turing Award

---
+++ footer.js
@@ -0,0 +1,11 @@
+document.write('<footer>\n' +
+    '    <div id="bottom">\n' +
+    '        <table align="center" valign="bottom">\n' +
+    '            <tr>\n' +
+    '                <td align="center" valign="center">\n' +
+    '                    <p><font color="white">2017</font></p>\n' +
+    '                    <p><font color="white">Проект студентов группы БВТ1501 Шарикова Алексея и Иванниковой Валентины</font></p>\n' +
+    '                </td>\n' +
+    '            </tr>\n' +
+    '        </table>\n' +
+    '    </div></footer>')
---
+++ header.js
@@ -0,0 +1,3 @@
+document.write('<div id="top" class="aref" align="center">\n' +
+    '    <font color="white" size="5"><a href="../index.html" style="color: white"><b>Премия Тьюринга</b></a></font><br>\n' +
+    '</div>')
