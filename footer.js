02d7de8e8ab8c819655e9f87e7a5edd6f17775bd
Derelanin <solo.akicare@gmail.com>
2018-01-28 14:06:31 +0300

    2000 Andrew Chi-Chih Yao

--- footer.js
+++ footer.js
@@ -3,7 +3,7 @@ document.write('<footer>\n' +
     '        <table align="center" valign="bottom">\n' +
     '            <tr>\n' +
     '                <td align="center" valign="center">\n' +
-    '                    <p><font color="white">2017</font></p>\n' +
+    '                    <p><font color="white">2017-2018</font></p>\n' +
     '                    <p><font color="white">Проект студентов группы БВТ1501 Шарикова Алексея и Иванниковой Валентины</font></p>\n' +
     '                </td>\n' +
     '            </tr>\n' +
